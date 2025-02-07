0f51f3e3355d53afb31209f8e823327cce362646
giuliaa06 <scarsi.giulia@itismeucci.com>
2025-02-07 10:46:34 +0100

    method addSecondi

--- src/Time.java
+++ src/Time.java
@@ -2,4 +2,51 @@ public class Time {
     protected int minuti;
     protected int ore;
     protected int secondi;
+
+
+    /**
+     * 
+     * @return minuti 
+     */
+    public int getMinuti() {
+        return minuti;
+    }
+
+    /**
+     * 
+     * @return ore 
+     */
+    public int getOre() {
+        return ore;
+    }
+
+    /**
+     * 
+     * @return secondi 
+     */
+    public int getSecondi() {
+        return secondi;
+    }
+
+/** La classe addSecondi addiziona il numero di secondi specificati, normalizzando il tempo in automatico
+ * 
+ * @param secondi
+ */
+
+public void addSecondi(int secondi) {
+        this.secondi += secondi;
+
+
+        while (this.secondi >= 60) {
+            this.secondi -= 60;
+            this.minuti++;
+        }
+
+      
+        while (this.minuti >= 60) {
+            this.minuti -= 60;
+            this.ore++;
+        }
+    }
+
 }
